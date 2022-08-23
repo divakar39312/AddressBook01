15c89548fee591cbca259fddf7fcdd4be6fd3180
Divakar <divakar39312@gmail.com>
2022-08-23 18:59:19 +0530

    [Divakar]:file added

---
+++ src/com/Bridgelabz/AddressBook.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		/**
+		 * Creating object of class Contact and calling the method
+		 */
+		System.out.println("<<< Welcome to Address Book System >>>");
+		System.out.println("======================================");
+		Contact C = new Contact();
+		C.contacts();
+	}
+}
