d0e4f8d265049eeb122d280d274c70974c8894d8
Divakar <divakar39312@gmail.com>
2022-08-23 19:10:30 +0530

    [Divakar]:UC2 create a new contact

---
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		//creating a object for addressbook
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
+		addressBook.displayContact();
+	}
+}
